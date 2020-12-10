3d9f0525ed5b74dd452f5baa80d557b887f9694e
Dell <Dell@DESKTOP-SFLMERV>
2020-12-10 12:26:18 +0530

    added arthmetic class

--- HelloGit/src/hellodemo/HelloWorld.java
+++ HelloGit/src/hellodemo/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!!");
+		ArthClass obj=new ArthClass();
+		int num=obj.add(10, 20);
+		System.out.println("add of two nums:"+(num));
 	}
 
 }
